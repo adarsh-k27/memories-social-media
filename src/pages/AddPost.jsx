5a63baac761fd996069746352bf40648fcb50e87
adarsh-k27 <adarshkdev27@gmail.com>
2022-10-30 12:19:42 +0530

    add post section processing

--- src/pages/AddPost.jsx
+++ src/pages/AddPost.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
-
+import {InputBox} from '../components'
 function AddPost() {
   return (
-    <div>AddPost</div>
+    <div className='w-screen h-auto flex flex-col justify-center items-center '>
+      <div className='flex flex-col gap-5 py-3 px-5'>
+          <h2>Add Post</h2>
+           <InputBox text="Title"   />
+           <InputBox text="Short Description"   />
+           <InputBox text="Description" />
+           <InputBoxt text="Enter Tags Here" />
+      </div>
+    </div>
   )
 }
 
