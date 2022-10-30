0b4e55077dadc4e316b8959aea2f1a0bfdbc87fb
adarsh-k27 <adarshkdev27@gmail.com>
2022-10-30 13:25:16 +0530

    add image section

--- src/components/ImageLoader.jsx
+++ src/components/ImageLoader.jsx
@@ -1,26 +1,29 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { BsFillCloudArrowDownFill } from '../icons'
-
-function ImageLoader({loading,imageUrl}) {
+function ImageLoader ({ loading, imageUrl }) {
+  const File = useRef()
   return (
     <div className='w-full h-full py-1 border-2 border-solid border-gray-500 flex flex-col items-center justify-center'>
-      {
-        loading ? (
-            <div><p>Loading</p></div>
-        )
-        : !imageUrl ? (
-            <div className=''>
-                <label htmlFor=''>
-                <input type='file' name='' id='' className='hidden' />
-                <BsFillCloudArrowDownFill />
-                </label>
-
-            </div>
-        )
-        :(
-            <div>Image here</div>
-        )
-      }
+      {loading ? (
+        <div>
+          <p>Loading</p>
+        </div>
+      ) : !imageUrl ? (
+        <div
+          className='w-full h-full flex items-center justify-center'
+          onClick={() => {
+            File.current.click()
+          }}
+        >
+          <label htmlFor=''>
+            <input type='file' name='' id='' className='hidden' ref={File} />
+            <BsFillCloudArrowDownFill className='text-6xl text-gray-600' />
+          </label>
+          <p>Choose Image</p>
+        </div>
+      ) : (
+        <div>Image here</div>
+      )}
     </div>
   )
 }
--- src/pages/AddPost.jsx
+++ src/pages/AddPost.jsx
@@ -3,15 +3,15 @@ import { ImageLoader, InputBox, TextArea } from '../components'
 function AddPost () {
   return (
     <div className='w-screen h-auto flex flex-col justify-center items-center '>
-      <div className='w-full grid grid-cols-2 gap-5'>
-        <h2>Add Post</h2>0
+      <div className='w-full grid grid-cols-1 md:grid-cols-2 gap-5'>
         <div>
+          <h2>Add Post</h2>
           <InputBox text='Title' />
           <TextArea text='Short Description' row={4} />
           <TextArea text='Description' row={10} />
         </div>
         <div>
-          <ImageLoader loading={true} />
+          <ImageLoader loading={false} imageUrl={true}  />
         </div>
       </div>
     </div>
