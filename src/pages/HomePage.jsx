fda937ff35ad5f48a345cedb281856d170bb7939
adarsh-k27 <adarshkdev27@gmail.com>
2022-11-17 15:49:48 +0530

    bottom bar fixing

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,12 @@
 import React, { useEffect } from 'react'
 import HomeWrapper from '../components/HomeWrapper'
-import { BottomBar, ReactCard, SearchBox, SideBar } from '../components'
+import {
+  BottomBar,
+  Pagination,
+  ReactCard,
+  SearchBox,
+  SideBar
+} from '../components'
 import { useSelector } from 'react-redux'
 import { FetchPosts } from '../api/user'
 function HomePage () {
@@ -11,10 +17,11 @@ function HomePage () {
   }, [page])
   return (
     <HomeWrapper>
-      <div className='w-full h-screen grid grid-rows-[85%,15%] md:grid-cols-[75%,25%] mx-2'>
+      <div className='w-full h-screen grid grid-rows-[90%,10%] md:grid-cols-[75%,25%] mx-2'>
         <div className='flex flex-col gap-3'>
-          <div className='block md:hidden'>
+          <div className='FLEX flex-col gap-2 md:hidden'>
             <SearchBox />
+            <Pagination />
           </div>
           <div className='flex flex-wrap items-center justify-start gap-6 overflow-y-scroll'>
             {posts.length > 0 &&
@@ -23,11 +30,11 @@ function HomePage () {
         </div>
         <div>
           <div className='hidden md:block'>
-           <SideBar />
-        </div>
-        <div className='block md:hidden'>
-          <BottomBar />
-        </div>
+            <SideBar />
+          </div>
+          <div className='block md:hidden'>
+            <BottomBar />
+          </div>
         </div>
       </div>
     </HomeWrapper>
